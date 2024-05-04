e6d05756eddc619864626177b1da38b4d0a909d9
pranzalkhadka <pranzalkhadka1@gmail.com>
2024-05-04 18:58:52 +0545

    fastapi for application

--- app.py
+++ app.py
@@ -1,3 +1,66 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI, File, UploadFile
+# from fastapi.middleware.cors import CORSMiddleware
+# import keras
+# import io
+# from fastapi.responses import StreamingResponse
+# import tensorflow as tf
+# import PIL
+
+# image_height = 256
+# image_width = 256
+
+# model = tf.keras.models.load_model('/home/pranjal/Downloads/Building_footprint_segmentation/model1.keras')
+
+# app = FastAPI()
+
+# @app.get("/")
+# async def process_file():
+#     return {"filename": "file"}
+
+
+
+# @app.post("/predict")
+# async def process_image(file: UploadFile = File(...)):
+#     # Your image processing logic here
+#     return {"filename": file.filename}
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import keras
+import numpy as np
+import io
+from fastapi.responses import StreamingResponse
+import tensorflow as tf
+import PIL
+import cv2
+
+image_height = 256
+image_width = 256
+
+model = tf.keras.models.load_model('/home/pranjal/Downloads/Building_footprint_segmentation/model1.keras')
 
 app = FastAPI()
+
+@app.get("/")
+async def process_file():
+    return {"filename": "file"}
+
+
+@app.post("/predict")
+async def process_image(file: UploadFile = File(...)):
+    content = await file.read()
+    img = cv2.imread(content)
+    test_image_resized = cv2.resize(img, (image_height, image_width))
+    test_image_gray = cv2.cvtColor(test_image_resized, cv2.COLOR_BGR2GRAY)
+    test_image_normalized = test_image_gray / 255.0
+    test_image_input = np.expand_dims(test_image_normalized, axis=-1)
+    test_image_input = np.expand_dims(test_image_input, axis=0)
+
+    predicted_mask = model.predict(test_image_input)
+
+    predicted_mask = predicted_mask.reshape(image_height, image_width)
+    predicted_mask_binary = np.where(predicted_mask > 0.5, 1, 0)
+
+    return StreamingResponse(io.BytesIO(predicted_mask_binary), media_type="image/png")
+
